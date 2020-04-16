f3ba425d2ba0cea42f6f37e151ebef51e31fe135
FelipeRodrigues <felipe.rodrigues@keyrus.com.br>
2020-04-16 20:16:48 -0300

    by pass

--- src/main/java/br/com/ldap/service/LdapService.java
+++ src/main/java/br/com/ldap/service/LdapService.java
@@ -1,29 +1,61 @@
 package br.com.ldap.service;
 
+import org.apache.http.conn.ssl.NoopHostnameVerifier;
+import org.apache.http.conn.ssl.SSLConnectionSocketFactory;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.ssl.SSLContexts;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.*;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+
+import javax.naming.directory.DirContext;
+import javax.net.ssl.HostnameVerifier;
+import javax.net.ssl.HttpsURLConnection;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.SSLSession;
+import java.net.URL;
+import java.util.Arrays;
 
 
 @Service("ldapservice")
 public class LdapService {
 
-    private  static String URL;
+    private static String URLL;
 
     public ResponseEntity retrieveLdapInformation() {
 
-        RestTemplate restTemplate = new RestTemplate();
-        HttpEntity<Object> httpEntity = restTemplate.exchange(URL, HttpMethod.POST, null, Object.class);
-        return ResponseEntity.ok(httpEntity.getBody());
+        HttpEntity<DirContext> httpEntity = null;
 
+        try {
+
+            HttpComponentsClientHttpRequestFactory requestFactory = new HttpComponentsClientHttpRequestFactory();
+            SSLContext sslcontext = SSLContexts.custom().loadTrustMaterial(null, (chain, authType) -> true).build();
+            SSLConnectionSocketFactory sslsf = new SSLConnectionSocketFactory(sslcontext, new String[]{"TLSv1"}, null, new NoopHostnameVerifier());
+            CloseableHttpClient httpclient = HttpClients.custom().setSSLSocketFactory(sslsf).build();
+            requestFactory.setHttpClient(httpclient);
+            RestTemplate restTemplate = new RestTemplate(requestFactory);
+
+            final HttpHeaders headers = new HttpHeaders();
+            headers.setContentType(MediaType.APPLICATION_JSON);
+            headers.setAccept(Arrays.asList(MediaType.APPLICATION_JSON));
+
+            httpEntity  =   restTemplate.exchange(URLL, HttpMethod.POST, new HttpEntity<>(headers), DirContext.class);
+
+
+        } catch (Exception cause) {
+
+            System.out.println(cause);
+        }
+
+        return ResponseEntity.ok(httpEntity.getBody());
     }
 
     @Value("${hybris.url.security}")
-    public void setURL(String URL) {
-        this.URL = URL;
+    public void setURL(String URLL) {
+        this.URLL = URLL;
     }
 }
